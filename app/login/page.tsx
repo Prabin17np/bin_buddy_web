602789494acad8e51166e6ddd6c89bdddfae6572
Prabin Giri <prabin17np@gmail.com>
2025-12-26 20:48:59 +0545

    setting register folder

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import LoginForm from "../_components/LoginForm";
+
+export default function Page() {
+    return (
+        <div>
+            <div>
+                <h1>Welcome back</h1>
+                <p>Log in to your account</p>
+            </div>
+            <LoginForm />
+        </div>
+    );
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import RegisterForm from "../_components/RegisterForm";
+
+export default function Page() {
+    return (
+        <div>
+            <div>
+                <h1>Create your account</h1>
+                <p>Sign up to get started</p>
+            </div>
+            <RegisterForm />
+        </div>
+    );
+}
