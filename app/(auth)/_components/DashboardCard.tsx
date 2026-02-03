9f13c761015a6f92e52c14313b6a32e15a80d805
Prabin Giri <prabin17np@gmail.com>
2026-02-03 18:06:00 +0545

    add user dashboard page (page.tsx) layout

--- app/(auth)/_components/DashboardCard.tsx
+++ app/(auth)/_components/DashboardCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 type Props = {
   title: string;
   value: string;
@@ -7,12 +8,8 @@ type Props = {
 export default function DashboardCard({ title, value }: Props) {
   return (
     <div className="rounded-xl bg-white p-6 shadow">
-      <p className="text-sm text-gray-500">
-        {title}
-      </p>
-      <h2 className="mt-2 text-3xl font-bold text-gray-900">
-        {value}
-      </h2>
+      <p className="text-sm text-gray-500">{title}</p>
+      <h2 className="mt-2 text-3xl font-bold text-gray-900">{value}</h2>
     </div>
   );
 }
