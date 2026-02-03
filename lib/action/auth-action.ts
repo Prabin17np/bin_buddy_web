75241565f34ddb8d2a9b73366aff9eeb77f0d649
Prabin Giri <prabin17np@gmail.com>
2026-02-03 18:04:54 +0545

    implement user and admin actions for CRUD and auth flows

--- lib/action/auth-action.ts
+++ lib/action/auth-action.ts
@@ -3,7 +3,8 @@
 
 "use server";
 
-import { register, login } from "../api/auth";
+import { revalidatePath } from "next/cache";
+import { register, login, updateUser } from "../api/auth";
 import { setAuthToken, setUserData } from "../cookie";
 
 export const handleRegister = async (formData: any) => {
@@ -46,3 +47,26 @@ export const handleLogin = async (formData: any) => {
     return {success: false, message: error.message || "Login failed"};
   }
 }
+
+export const handleUpdateUser = async (data: FormData) => {
+  try {
+    const response = await updateUser(data);
+    if (response.success) {
+      revalidatePath("/user/profile");
+      return {
+        success: true,
+        message: "Update successful",
+        data: response.data,
+      };
+    }
+    return {
+      success: false,
+      message: response.message || "Update failed",
+    };
+  } catch (error: Error | any) {
+    return {
+      success: false,
+      message: error.message || "Update action failed",
+    };
+  }
+};
